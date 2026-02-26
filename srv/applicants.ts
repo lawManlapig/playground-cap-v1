870aa55c1b1bcfb73df47aecf5463503c675f5b9
Law Manlapig <hehehelaw@gmail.com>
2026-02-26 07:29:09 +0000

    Added Default Values

--- srv/applicants.ts
+++ srv/applicants.ts
@@ -3,47 +3,49 @@ import * as cds from "@sap/cds";
 export class ApplicantsService extends cds.ApplicationService {
   async init() {
     // Real-time updates (Drafts)
-    this.before(
-      "UPDATE",
-      "Applicants.drafts",
-      async (req) => {
-        let statusCriticality = 0;
-        let draftData = await SELECT.one("")
-          .from("ApplicantsService_Applicants_drafts")
-          .where({ ID: req.data.ID });
+    this.before("CREATE", "Applicants.drafts", async (req) => {
+      req.data.status = "New";
+    });
 
-        switch (req.data.status) {
-          case "New":
-            statusCriticality = 0;
-            break;
-          case "Rejected":
-            statusCriticality = 1;
-            break;
-          case "Whitelisted":
-            statusCriticality = 2;
-            break;
-          case "Accepted":
-            statusCriticality = 3;
-            break;
-          default:
-            break;
-        }
+    this.before("UPDATE", "Applicants.drafts", async (req) => {
+      let statusCriticality = 0;
+      let draftData = await SELECT.one("")
+        .from("ApplicantsService_Applicants_drafts")
+        .where({ ID: req.data.ID });
 
-        let draftPayload = {
-          DRAFTADMINISTRATIVEDATA_DRAFTUUID:
-            draftData.DRAFTADMINISTRATIVEDATA_DRAFTUUID,
-          ID: req.data.ID,
-          STATUSCRITICALITY: statusCriticality,
-        };
+      switch (req.data.status) {
+        case "New":
+          statusCriticality = 0;
+          break;
+        case "Rejected":
+          statusCriticality = 1;
+          break;
+        case "Whitelisted":
+          statusCriticality = 2;
+          break;
+        case "Accepted":
+          statusCriticality = 3;
+          break;
+        default:
+          break;
+      }
 
-        // Update the draft table
-        await UPDATE("ApplicantsService_Applicants_drafts").set(draftPayload).where({
+      let draftPayload = {
+        DRAFTADMINISTRATIVEDATA_DRAFTUUID:
+          draftData.DRAFTADMINISTRATIVEDATA_DRAFTUUID,
+        ID: req.data.ID,
+        STATUSCRITICALITY: statusCriticality,
+      };
+
+      // Update the draft table
+      await UPDATE("ApplicantsService_Applicants_drafts")
+        .set(draftPayload)
+        .where({
           ID: req.data.ID,
           DRAFTADMINISTRATIVEDATA_DRAFTUUID:
             draftData.DRAFTADMINISTRATIVEDATA_DRAFTUUID,
         });
-      },
-    );
+    });
 
     return super.init();
   }
